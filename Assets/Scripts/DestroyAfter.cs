101a552eb74c1c24b76b89ff6f102ca78ca1c638
Lukas Wiedemann <lukaswiedemann@gmail.com>
2018-10-11 14:44:39 +0200

    Refactored code

--- Assets/Scripts/DestroyAfter.cs
+++ Assets/Scripts/DestroyAfter.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Used to destroy the attached Object
+/// </summary>
 public class DestroyAfter : MonoBehaviour {
-
+    /// <summary>The time after which the object is destroyed</summary>
     [SerializeField]
     private float destroyTime;
-	
-	// Update is called once per frame
-	void Update () {
+
+    /// <summary>Update is called once per frame</summary>
+    void Update() {
         destroyTime -= Time.deltaTime;
         if (destroyTime <= 0f) {
             Destroy(gameObject);
