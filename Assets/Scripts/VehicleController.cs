8500bdd3b276bebd18b0582f56553d9f9ea3681b
Unknown <janik.hofmann@games-academy.de>
2018-10-11 13:38:33 +0200

    Implemented Polybrush and Map Prototype

---
+++ Assets/Scripts/VehicleController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class VehicleController : MonoBehaviour
+{
+    [SerializeField]
+    private float movementSpeed;
+
+    [SerializeField]
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Movement();
+    }
+
+    void Movement() {
+        Vector2 rotation = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal"), CrossPlatformInputManager.GetAxis("Vertical"));
+
+        if (rotation == Vector2.zero) return;
+        
+        var rotate = Vector2.Angle(Vector2.up, rotation);
+        var sRotate = rotate;
+
+        if(CrossPlatformInputManager.GetAxis("Horizontal") <= 0) {
+            rotate = -rotate;
+        } else {
+            rotate = sRotate;
+        }
+
+        this.transform.localEulerAngles = new Vector3(0, rotate, 0);
+        this.rb.velocity = this.transform.forward * rotation.magnitude;
+    }
+}
---
+++ Assets/Scripts/VehicleWeapon.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class VehicleWeapon : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject weaponType;
+
+    [SerializeField]
+    private Transform shotSpawn;
+
+    [SerializeField]
+    private float projectileSpeed;
+
+    [SerializeField]
+    private float shootingTime = 0.5f;
+
+    private float aktShootingTime;
+
+	// Use this for initialization
+	void Start () {
+        aktShootingTime = shootingTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Shoot(weaponType);
+	}
+
+    void Shoot(GameObject ammo) {
+        if(aktShootingTime <= 0f && (CrossPlatformInputManager.GetButton("Shoot"))) {
+            var shot = Instantiate(ammo, shotSpawn.position, ammo.transform.rotation);
+            shot.GetComponent<Rigidbody>().AddForce(this.transform.forward*projectileSpeed);
+            aktShootingTime += shootingTime;
+        }else if (CrossPlatformInputManager.GetButton("Shoot")) {
+            aktShootingTime -= Time.deltaTime;
+        }
+    }
+}
