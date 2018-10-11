92a3e45d47118dfe73258aa64e3d2d6f5e463638
Lukas Wiedemann <lukaswiedemann@gmail.com>
2018-10-11 14:39:26 +0200

    Refactored code and updated coding conventions

--- Assets/Scripts/Turrets/TurretController.cs
+++ Assets/Scripts/Turrets/TurretController.cs
@@ -2,23 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Controlls movement and shooting of a turret
+/// </summary>
 public class TurretController : MonoBehaviour {
+    /// <summary>The bullet prefab to spawn</summary>
+    [SerializeField]
+    private readonly GameObject bulletPrefab;
 
+    /// <summary>Force for bullets is multiplied by this</summary>
+    [SerializeField]
+    private readonly float bulletForce;
+
+    /// <summary>The time it takes for the turret to shoot again</summary>
+    [SerializeField]
+    private readonly float shootingTime;
+
+    /// <summary>The transform from which the turret shoots and the range calculation is done</summary>
     [SerializeField]
     private Transform shootingPoint;
-    [SerializeField]
-    private GameObject bulletPrefab;
-    [SerializeField]
-    private float bulletForce;
-    [SerializeField]
-    private float shootingTime;
-    
-    private GameObject[] playersGameObjects;
+
+    /// <summary>The transform off all GameObjects with a player tag</summary>
     private Transform[] players;
+
+    /// <summary>The time until the next shot occurs</summary>
     private float aktShootingTime;
 
-    // Use this for initialization
-    void Start () {
+    /// <summary>Use this for initialization</summary>
+    void Start() {
+        GameObject[] playersGameObjects;
         aktShootingTime = shootingTime;
         playersGameObjects = GameObject.FindGameObjectsWithTag("Player");
         players = new Transform[playersGameObjects.Length];
@@ -26,28 +38,36 @@ public class TurretController : MonoBehaviour {
             players[i] = playersGameObjects[i].transform;
         }
 
-        //Sort Players by Magnitude
+        // Sort Players by Magnitude
         OrderByMagnitude(ref players, shootingPoint);
     }
-	
-	// Update is called once per frame
-	void Update () {
-        //Sort Players by Magnitude
+
+    /// <summary>Update is called once per frame</summary>
+    void Update() {
+        // Sort Players by Magnitude
         OrderByMagnitude(ref players, shootingPoint);
         aktShootingTime -= Time.deltaTime;
         if (aktShootingTime <= 0f) {
-            ShootAtEnemy(players[0]); //Target (nearest)
+            // Target (nearest)
+            ShootAtEnemy(players[0]);
             aktShootingTime += shootingTime;
         }
-
     }
 
+    /// <summary>
+    /// Orders given transforms by magnitude to another transform
+    /// </summary>
+    /// <param name="transforms">Transforms to order</param>
+    /// <param name="referenceTransform">Transform for magnitude check</param>
     void OrderByMagnitude(ref Transform[] transforms, Transform referenceTransform) {
         var hasSorted = true;
         Transform switchTransform;
         for (int i = 0; hasSorted; i++) {
             hasSorted = false;
-            if (transforms.Length <= 1) return;
+            if (transforms.Length <= 1) {
+                return;
+            }
+
             for (int i2 = 1; i2 < transforms.Length; i2++) {
                 var dist1 = Vector3.SqrMagnitude(referenceTransform.position - transforms[i2 - 1].position);
                 var dist2 = Vector3.SqrMagnitude(referenceTransform.position - transforms[i2].position);
@@ -61,10 +81,13 @@ public class TurretController : MonoBehaviour {
         }
     }
 
+    /// <summary>
+    /// Shoots at the targeted transform
+    /// </summary>
+    /// <param name="target">The target to shoot at</param>
     void ShootAtEnemy(Transform target) {
         Debug.DrawLine(shootingPoint.position, target.position, Color.red, 0.05f);
         var bullet = Instantiate(bulletPrefab, shootingPoint.position, shootingPoint.rotation, null);
         bullet.GetComponent<Rigidbody>().AddForce((target.position - bullet.transform.position) * bulletForce);
     }
-
 }
