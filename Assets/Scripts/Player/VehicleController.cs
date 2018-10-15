f5b951ad5da7a60fc5ac2a5435d5fdeb92612ef0
Lukas Wiedemann <lukaswiedemann@gmail.com>
2018-10-15 17:25:05 +0200

    Implemented new movement with turns

--- Assets/Scripts/Player/VehicleController.cs
+++ Assets/Scripts/Player/VehicleController.cs
@@ -12,41 +12,46 @@ public class VehicleController : MonoBehaviour
     [SerializeField]
     private float movementSpeed;
 
-    /// <summary>The vehicles rigidbody</summary>
+    /// <summary>How many degrees per second the car can turn</summary>
     [SerializeField]
+    private float degreePerSecond;
+
+    /// <summary>The vehicles rigidbody</summary>
     private Rigidbody rb;
+
+    /// <summary>The rotation goal</summary>
+    private Vector2 goalRotate;
 
     /// <summary>
     /// Use this for initialization
     /// </summary>    
     void Start() {
+        rb = gameObject.GetComponent<Rigidbody>();
     }
 
     /// <summary>
     /// Update is called once per frame
     /// </summary>    
     void Update() {
-        Movement();
+        Movement(CrossPlatformInputManager.GetAxis("Horizontal"), CrossPlatformInputManager.GetAxis("Vertical"));
     }
 
     /// <summary>
     /// Uses CrossplatformInput to move and rotate player vehicle
     /// </summary>
-    void Movement() {
-        Vector2 rotation = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal"), CrossPlatformInputManager.GetAxis("Vertical"));
+    void Movement(float horizontalAxis, float verticalAxis) {
+        float rotate;
+        var rotation = new Vector2(horizontalAxis, verticalAxis);
         if (rotation == Vector2.zero) {
             return;
         }
 
-        var rotate = Vector2.Angle(Vector2.up, rotation);
-        var saveRotate = rotate;
-        if (CrossPlatformInputManager.GetAxis("Horizontal") <= 0) {
-            rotate = -rotate;
-        } else {
-            rotate = saveRotate;
-        }
-
-        this.transform.localEulerAngles = new Vector3(0, rotate, 0);
-        this.rb.velocity = this.transform.forward * rotation.magnitude * movementSpeed;
+        goalRotate = rotation;
+        rotate = Vector2.SignedAngle(goalRotate, Vector2.up);
+        var quat = new Quaternion {
+            eulerAngles = new Vector3(0, rotate, 0)
+        };
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, quat, degreePerSecond * rotation.magnitude * Time.deltaTime);
+        rb.velocity = transform.forward * rotation.magnitude * movementSpeed;
     }
 }
