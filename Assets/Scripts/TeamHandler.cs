91ad116a8ec4fc10d99c0af4c2a8dd3cca421240
Unknown <janik.hofmann@games-academy.de>
2018-10-22 15:17:11 +0200

    Added Tower prefab + refactored Code + Minion-Prototype

--- Assets/Scripts/TeamHandler.cs
+++ Assets/Scripts/TeamHandler.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class TeamHandler : MonoBehaviour {
 
     /// <summary>This units team-ID</summary>
-    [Tooltip("0 = Friendly, 1 = Enemy, 2 = Neutral")]
-    public byte teamID = 0;
+    [SerializeField]
+    private TeamState teamID = 0;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public enum TeamState
+    {
+        FRIENDLY = 0,
+        ENEMY = 1,
+        NEUTRAL = 2
+    };
+
+    public TeamState TeamID {
+        get { return teamID; }
+        set { teamID=value; }
+    }
 }
