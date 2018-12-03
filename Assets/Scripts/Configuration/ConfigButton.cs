babf344e7fa11fbb53e411007203008f1769bdf9
Lukas Wiedemann <lukaswiedemann@gmail.com>
2018-12-03 09:54:16 +0100

    Included ini-parser to read configs
    ini-parser by rickyah used with MIT License

---
+++ Assets/Scripts/Configuration/ConfigButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using IniParser;
+using IniParser.Model;
+
+public class ConfigButton : MonoBehaviour {
+
+    public void OnButtonPress() {
+        var parser = new FileIniDataParser();
+        var data = parser.ReadFile("Config.ini");
+        
+        foreach (var section in data.Sections) {
+            Debug.Log("" + section.SectionName +"");
+            foreach (var key in section.Keys) {
+                Debug.Log(key.KeyName + " = " + key.Value);
+            }
+        }
+    }
+
+    /// <summary>Use this for initialization</summary>
+    void Start() {
+
+    }
+
+    /// <summary>Update is called once per frame</summary>
+    void Update() {
+        
+    }
+}
