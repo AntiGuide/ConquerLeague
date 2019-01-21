d28bb3d1b690924a8a2f597e895172153c6f5a4f
Unknown <janik.hofmann@games-academy.de>
2019-01-21 14:26:47 +0100

    Added more Wasteland Package Assets + Main Menu Scene

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -3,12 +3,64 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MenuManager : MonoBehaviour {
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField]
+    private Transform cameraDestinationGarage;
+
+    [SerializeField]
+    private Transform cameraDestinationBattle;
+
+    [SerializeField]
+    private float cameraSpeed = 1;
+
+    [SerializeField]
+    private Camera mainCamera;
+
+    private Transform cameraDestination;
+    private bool moveToGarage = false;
+    private bool cameraMoving;
+    private float movingTimer = 0;
+    private RaycastHit hit;
+    private Ray ray;
+
+    private Transform cameraStartTrans;
+
+    private void Start() {
+        cameraStartTrans = transform;
+    }
+
+    private void Update() {
+        if (Input.GetMouseButtonDown(0) && !cameraMoving) {
+            ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 100, LayerMask.GetMask("ButtonMainMenu"))) {
+                cameraMoving = true;
+                cameraDestination = hit.transform.gameObject.CompareTag("Garage") ? cameraDestinationGarage : cameraDestinationBattle;
+            }
+        }
+
+        if (cameraMoving) {
+            movingTimer += Time.deltaTime * cameraSpeed;
+            LerpTransform(cameraStartTrans, cameraDestination, movingTimer);
+
+            if (movingTimer >= 1) {
+                cameraMoving = false;
+                movingTimer = 0;
+            }
+        }
+    }
+
     public void OnClickMainMenue() {
         SceneManager.LoadScene(0);
     }
 
     public void OnClickStartGame() {
         SceneManager.LoadScene(1);
+    }
+
+    private void LerpTransform(Transform startTrans, Transform goalTrans, float movingTimer) {
+        transform.position = Vector3.Lerp(startTrans.position, goalTrans.position, movingTimer);
+        transform.rotation = Quaternion.Lerp(transform.rotation, goalTrans.rotation, movingTimer);
     }
 }
