79db4a3ec5077626a3f7fb7cc8c75ea59f2bb4b1
Lukas Wiedemann <lukaswiedemann@gmail.com>
2019-01-23 14:13:23 +0100

    Disconnect on scene load

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -5,10 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour {
     public void OnClickMainMenue() {
+        CommunicationNet.FakeStatic.client.Disconnect("OnClickMainMenue");
         SceneManager.LoadScene(0);
     }
 
     public void OnClickStartGame() {
+        CommunicationNet.FakeStatic.client.Disconnect("OnClickStartGame");
         SceneManager.LoadScene(1);
     }
 }
