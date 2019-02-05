a04058d83f4d145f406c5df28b6027980bac558a
Unknown <janik.hofmann@games-academy.de>
2019-02-05 11:49:30 +0100

    RamCollider-Fix

--- Assets/Scripts/Player/RamCollider.cs
+++ Assets/Scripts/Player/RamCollider.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RamCollider : MonoBehaviour {
+    [SerializeField]
+    private TeamHandler teamHandler;
+
     private void OnTriggerEnter(Collider other) {
-        if (other.gameObject.GetComponent<HitPoints>() != null) {
+        if (other.gameObject.GetComponent<HitPoints>() != null 
+            && other.gameObject.GetComponent<TeamHandler>()?.TeamID != teamHandler.TeamID 
+            && other.gameObject.GetComponent<TeamHandler>()?.TeamID != TeamHandler.TeamState.NEUTRAL) {
             other.gameObject.GetComponent<HitPoints>().AktHp -= other.gameObject.GetComponent<HitPoints>().AktHp;
         }
     }
