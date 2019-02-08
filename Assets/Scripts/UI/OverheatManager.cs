6dd38d81b3fdc30d47285f57a22ea3aa1625e30f
Unknown <janik.hofmann@games-academy.de>
2019-02-08 10:00:56 +0100

    Change Overheatbar Color when cooling down

--- Assets/Scripts/UI/OverheatManager.cs
+++ Assets/Scripts/UI/OverheatManager.cs
@@ -23,6 +23,8 @@ public class OverheatManager : MonoBehaviour, IConfigurable {
 
     private bool inactivityCooldown = true;
 
+    private float blinkingImageTimer;
+
     public enum HeatState {
         SHOOTABLE,
         COOLING
@@ -68,6 +70,14 @@ public class OverheatManager : MonoBehaviour, IConfigurable {
     }
 
     private void Update() {
+        if(state == HeatState.COOLING) {
+            blinkingImageTimer += Time.deltaTime;
+            overheatFullImage.color = Color.Lerp(Color.white, Color.blue, blinkingImageTimer);
+        } else {
+            blinkingImageTimer = 0;
+            overheatFullImage.color = Color.white;
+        }
+
         if (state == HeatState.COOLING || (inactivityCooldown && state == HeatState.SHOOTABLE)) {
             overheatPercentage -= cooldownPerSecond * Time.deltaTime;
             if (overheatPercentage <= 0f) {
