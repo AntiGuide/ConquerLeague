a02e2c4eb521fd15045560bbf87417f2a283113d
Unknown <janik.hofmann@games-academy.de>
2019-02-12 11:54:53 +0100

    MinionCloth

---
+++ Assets/Scripts/Minions/MinionCloth.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinionCloth : MonoBehaviour {
+    [SerializeField]
+    private TeamHandler teamHandler;
+
+    [SerializeField]
+    private Material matBlue;
+
+    [SerializeField]
+    private Material matRed;
+
+    private bool isEnemy;
+
+	// Use this for initialization
+	void Start () {
+        if(teamHandler.TeamID == TeamHandler.TeamState.FRIENDLY) {
+            GetComponent<MeshRenderer>().material = matBlue;
+        } else if (teamHandler.TeamID == TeamHandler.TeamState.ENEMY) {
+            GetComponent<MeshRenderer>().material = matRed;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
