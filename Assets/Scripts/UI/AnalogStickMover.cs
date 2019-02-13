9b75b235925aff5b29a3ba6dd88c57b64e711841
Lukas Wiedemann <lukaswiedemann@protonmail.com>
2019-02-13 10:31:21 +0100

    Move analog stick in Windows build

---
+++ Assets/Scripts/UI/AnalogStickMover.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.CrossPlatformInput;
+
+[RequireComponent(typeof(Image), typeof(RectTransform))]
+public class AnalogStickMover : MonoBehaviour {
+
+    private Image image;
+    private RectTransform trans;
+    private Vector2 startPos;
+
+    // Use this for initialization
+    void Start () {
+        image = GetComponent<Image>();
+        trans = GetComponent<RectTransform>();
+        startPos = trans.anchoredPosition;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        var v = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal"), CrossPlatformInputManager.GetAxis("Vertical"));
+        v = Vector2.ClampMagnitude(100 * v, 100f);
+        trans.anchoredPosition = startPos + v;
+    }
+}
